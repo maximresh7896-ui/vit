22160325bbf80c30a5e2360eb70c75deae84e3b4
Max <maxim.resh7896gmail.com>
2025-09-04 17:19:17 +0300

    coment

--- test.py
+++ test.py
@@ -1,2 +1,12 @@
 print("hello")
 print("world")
+
+import pyautogui
+import time
+time.sleep(5)
+for x in range(0,50,10):
+    pyautogui.dragTo(896, 712+x)
+    time.sleep(0.1)
+    pyautogui.dragTo(760, 442+x)
+    time.sleep(0.1)
+    pyautogui.dragTo(973, 470+x)
